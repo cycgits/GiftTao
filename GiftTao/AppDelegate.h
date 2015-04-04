247c13911a2f88c387a38a378b1b28121acfd43d
caiyc <caiyc@caiycdeMac-mini.local>
2015-04-04 13:51:54 +0800

    Initial Commit

---
+++ GiftTao/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GiftTao
+//
+//  Created by caiyc on 15/4/4.
+//  Copyright (c) 2015年 caiyc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GiftTao/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GiftTao
+//
+//  Created by caiyc on 15/4/4.
+//  Copyright (c) 2015年 caiyc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
